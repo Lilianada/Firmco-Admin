6bf22557a11d473b2edb0d06137c7c8088b3d283
Lilianada <lilibest30.lb@gmail.com>
2024-04-25 07:45:25 -0400

    add number index field to edit bond page

--- src/pages/Bonds/Edit.jsx
+++ src/pages/Bonds/Edit.jsx
@@ -232,6 +232,26 @@ export default function Edit() {
 
             <div className="sm:col-span-3">
               <label
+                htmlFor="index"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Number Index
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="index"
+                  id="index"
+                  value={formData.index}
+                  onChange={handleChange}
+                  autoComplete="index"
+                  className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
                 htmlFor="issuerName"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
